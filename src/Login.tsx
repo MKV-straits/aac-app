d7f1e67236fe7a38d24c414d452180fe10a206d1
Evan Bridgman-Fralin <fralinev@gmail.com>
2023-03-05 16:21:07 -0500

    add login code

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,17 +1,48 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import { useState } from "react";
+
 function Login() {
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = input;
+
+  function handleChange(event) {
+    console.log("handling change", input);
+    return setInput({ ...input, [event.target.name]: event.target.value });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("handling submit", input);
+  }
   return (
     <div>
       Login
-      <form className="login" method="post" id="login">
+      <form className="login" id="login" onSubmit={handleSubmit}>
         <label htmlFor="login" id="username">
           username
-          <input type="text" />
+          <input
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={username}
+          />
         </label>
 
         <br />
         <label htmlFor="login" id="password">
           password
-          <input type="text" />
+          <input
+            type="text"
+            name="password"
+            onChange={handleChange}
+            value={password}
+          />
+        </label>
+        <br />
+        <label htmlFor="submit" id="submit">
+          <input type="submit" name="submit" />
         </label>
       </form>
     </div>
